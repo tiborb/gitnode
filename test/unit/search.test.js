930cdc1c41e43aeaef8f922a22c34064910b9914
Tibor Barna <tibor.barna@wiredminds.de>
2017-01-17 10:21:15 +0100

    Corrected unit test name

--- test/unit/search.test.js
+++ test/unit/search.test.js
@@ -3,7 +3,7 @@ const Lab = require('lab');
 const lab = exports.lab = Lab.script();
 const User = require('../../src/entity/user');
 
-lab.test('returns true when 1 + 1 equals 2', (done) => {
+lab.test('user entity', (done) => {
     let user = new User('login', 'name', 'url', 2);
     Code.expect(user.username).to.exist();
     Code.expect(user.name).to.exist();
